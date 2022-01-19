531d7b9f429050935ac93850d7a34669add789cb
Matheus Cruz <matheuswachcruz@gmail.com>
2022-01-19 20:08:14 -0300

    feat: make request via server side rendering

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,28 +1,77 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
+import Head from "next/head";
+import {
+  Text,
+  Heading,
+  Flex,
+  IconButton,
+  Icon,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import { BiMenuAltLeft } from "react-icons/bi";
+
+// import { ThemeSwitcher } from "components/ThemeSwitcher";
+import { useSidebarMenuDrawer } from "contexts/SidebarMenuDrawerContext";
 
 import { supabase } from "services/supabase";
 
-const Skills: NextPage = () => {
-  const [programmingSkills, setProgrammingSkills] = useState([]);
+const Skills: NextPage = ({ programmingSkills, error }) => {
+  const { onOpen } = useSidebarMenuDrawer();
 
-  // TODO: Make request via server side rendering
-  useEffect(() => {
-    const fetchProgrammingSkills = async () => {
-      const { data: programmingSkills, error } = await supabase
-        .from("programming_skills")
-        .select("*");
+  const [isToShowOpenMenuButton] = useMediaQuery("(max-width: 800px)");
 
-      setProgrammingSkills(programmingSkills);
-    };
+  console.log({ programmingSkills, error });
 
-    fetchProgrammingSkills();
-  }, []);
+  return (
+    <>
+      <Head>
+        <title>Matheus da Cruz</title>
+      </Head>
 
-  useEffect(() => console.log({ programmingSkills }), [programmingSkills]);
-
-  return <Box>Skills</Box>;
+      <Flex
+        w="100%"
+        h="100%"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        position="relative"
+      >
+        {isToShowOpenMenuButton && (
+          <IconButton
+            aria-label="Open menu"
+            icon={<Icon as={BiMenuAltLeft} />}
+            fontSize="30"
+            mr="2"
+            color="white"
+            variant="unstyled"
+            top="3"
+            left="3"
+            position="absolute"
+            onClick={onOpen}
+          />
+        )}
+        <Heading>Skills</Heading>
+        {/* <ThemeSwitcher position="absolute" top="1.5" right="6" /> */}
+      </Flex>
+    </>
+  );
 };
 
 export default Skills;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: programmingSkills, error } = await supabase
+    .from("programming_skills")
+    .select("*");
+
+  console.log({ programmingSkills });
+
+  return {
+    props: {
+      programmingSkills,
+      error,
+    },
+    revalidate: 60 * 60 * 3, // = 3 horas
+  };
+};
