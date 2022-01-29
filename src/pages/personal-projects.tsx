6766186ae50a51f6c6d4b63c6199251aef8c7f4f
Matheus Cruz <matheuswachcruz@gmail.com>
2022-01-29 14:49:48 -0300

    feat: create Breadcrumb component

--- src/pages/personal-projects.tsx
+++ src/pages/personal-projects.tsx
@@ -7,15 +7,12 @@ import {
   Flex,
   IconButton,
   Icon,
-  useMediaQuery,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
   Box,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { BiMenuAltLeft } from "react-icons/bi";
-import { CgFormatSlash } from "react-icons/cg";
 
+import { Breadcrumb } from "components/Breadcrumb";
 import { useSidebarMenuDrawer } from "contexts/SidebarMenuDrawerContext";
 import { supabase } from "services/supabase";
 
@@ -77,24 +74,7 @@ const PersonalProjects: NextPage = ({
           />
         )}
         <Box position="absolute" top="16" left="3">
-          {/* TODO: componentize <Breadcrumb /> */}
-          <Breadcrumb
-            fontSize={["md", "md", "lg"]}
-            fontWeight="medium"
-            color="gray.200"
-            spacing="2px"
-            separator={<CgFormatSlash size={24} />}
-          >
-            <BreadcrumbItem>
-              <BreadcrumbLink isCurrentPage>Projects</BreadcrumbLink>
-            </BreadcrumbItem>
-
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/personal-projects">
-                Personal
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
+          <Breadcrumb currentPage="Personal" />
         </Box>
         <Heading>Personal Projects</Heading>
       </Flex>
