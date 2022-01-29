e1afaeba1702bd508d14c30ddac13bed34535fe0
Matheus Cruz <matheuswachcruz@gmail.com>
2022-01-28 21:37:57 -0300

    feat: development of skill UI page

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -8,6 +8,9 @@ import {
   Flex,
   IconButton,
   Icon,
+  Grid,
+  Image,
+  Progress,
   useMediaQuery,
 } from "@chakra-ui/react";
 import { BiMenuAltLeft } from "react-icons/bi";
@@ -44,7 +47,8 @@ const Skills: NextPage = ({ programmingSkills, error }: SkillsProps) => {
 
       <Flex
         w="100%"
-        h="100vh"
+        h={["100%", "100%", "100%", "100%", "100vh"]}
+        p="5"
         alignItems="center"
         justifyContent="center"
         flexDirection="column"
@@ -64,7 +68,89 @@ const Skills: NextPage = ({ programmingSkills, error }: SkillsProps) => {
             onClick={onOpen}
           />
         )}
-        <Heading>Skills</Heading>
+        <Flex mt="10" alignSelf="flex-start" flexDirection="column">
+          <Heading
+            fontSize={["3xl", "4xl", "5xl", "6xl"]}
+            color="blue.600"
+            fontWeight="bold"
+          >
+            Skills
+          </Heading>
+          <Text
+            mt="-10px"
+            fontSize={["lg", "lg", "xl"]}
+            fontWeight="normal"
+            color="gray.300"
+          >
+            that I&apos;ve been developing and perfecting myself daily
+          </Text>
+        </Flex>
+        <Grid
+          gap="10"
+          my="8"
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(3, 1fr)",
+          ]}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {programmingSkills?.map((skill) => (
+            <Flex
+              key={skill?.id}
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              gap="2"
+              p={["9", "8", "7", "6"]}
+              border="solid"
+              borderWidth="2px"
+              borderRadius="md"
+              borderColor="gray.300"
+              _hover={{ borderColor: "white" }}
+            >
+              <Image
+                alt={skill?.name}
+                src={skill?.icon}
+                height={["70px", "85px", "90px", "105px"]}
+                width={["70px", "85px", "90px", "105px"]}
+              />
+              <Text
+                fontSize={["md", "lg", "xl"]}
+                fontWeight="semibold"
+                color="gray.100"
+                textAlign="center"
+              >
+                {skill?.name}
+              </Text>
+              <Flex
+                w="100%"
+                gap="2"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Progress
+                  value={skill?.proficiency}
+                  max={100}
+                  min={1}
+                  size="sm"
+                  width="100%"
+                  bg="white"
+                  borderRadius="lg"
+                  colorScheme="cyan"
+                />
+                <Text
+                  fontWeight="bold"
+                  fontSize="smaller"
+                  color="gray.100"
+                >{`${skill?.proficiency}%`}</Text>
+              </Flex>
+            </Flex>
+          ))}
+        </Grid>
         {/* <ThemeSwitcher position="absolute" top="1.5" right="6" /> */}
       </Flex>
     </>
