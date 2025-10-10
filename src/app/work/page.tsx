349debafda3818b119ebdd3803e57c03e1d293d4
Cruz <matheuswachcruz@gmail.com>
2025-10-10 13:19:49 -0300

    fix: fix margin at work page mobile version

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -345,7 +345,7 @@ const Work = () => {
       }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
-      <div className="container mx-auto -mb-12 -mt-4">
+      <div className="container mx-auto xl:-mb-12 xl:-mt-4">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
